dbb1b13a41a7e108889022ec11b9347ad2c27ff2
Peter <pc.dean@icloud.com>
2020-02-07 14:46:42 +0000

    more refactoring, changed forcus from eliminating attack erb file

--- app.rb
+++ app.rb
@@ -6,23 +6,27 @@ class Battle < Sinatra::Base
     enable :sessions
 
     get '/' do
-        erb(:index)
+      erb(:index)
     end
 
     post '/names' do
-        player_1 = Player.new(params[:player1_name])
-        player_2 = Player.new(params[:player2_name])
-        $game = Game.new(player_1, player_2)
-        redirect('/play')
+      player_1 = Player.new(params[:player1_name])
+      player_2 = Player.new(params[:player2_name])
+      $game = Game.new(player_1, player_2)
+      redirect('/play')
     end
 
     get '/play' do
-        erb(:play)
+      erb(:play)
     end
 
     post '/attack' do
-       $game.attack
-       erb :attack
+      $game.attack
+      if $game.current_turn == $game.player2
+        erb :attack1
+      else
+        erb :attack2
+      end
     end
 
     # start the server if ruby file executed directly
